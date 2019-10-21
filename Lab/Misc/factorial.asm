399f2df14577873c3bec7d8d81ed16123217236b
Sabuj <sabujlovescoding@gmail.com>
2019-10-22 01:26:44 +0530

    FACTORIAL.ASM

--- Lab/Misc/factorial.asm
+++ Lab/Misc/factorial.asm
@@ -2,10 +2,22 @@ dosseg
 .model small
 .stack 100h
 .data
+	ANS DB ?
 .code
 main proc
 
+MOV AX,@DATA
+MOV DS,AX
 
+MOV AL,5
+MOV BL,4
+
+L1:
+MUL BL
+SUB BL,1
+LOOP L1
+
+MOV ANS,AL
 
 main endp
 end main
